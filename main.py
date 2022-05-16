d190565dd842ebab958a72b6a5d26c8c28160e57
xingzhe <54618163+2892211452@users.noreply.github.com>
2022-05-16 20:13:25 +0800

    更改目录结构

--- main.py
+++ main.py
@@ -1,86 +1,108 @@
-import  re
+import re
 import os
 import 加密算法.mymd5 as md5
 import requests
 from PIL import Image
 from Config import *
 
-
-
 # 这里是我的代理， 如果不需要代理删除这个就行，
-proxies = {"http":"http://127.0.0.1:7890", "https":"http://127.0.0.1:7890"}  #设置http和https 代理
+proxies = {"http": "http://127.0.0.1:7890", "https": "http://127.0.0.1:7890"}  # 设置http和https 代理
+
+# 转换后的图片url前缀
+github_url = None
+md_name = None
+md_name_hash = None
+content = None
+md_file = None
+img_dir_pre = None  # 图片存储的目录
+conf = get_config()
 
 
-os.makedirs( FileDir +'/image/', exist_ok=True)
-os.makedirs(FileDir +'/copyMD/', exist_ok=True)
-
-img_dir = FileDir +'/image/'
-
-githubUrl = 'https://github.com/2892211452/MDimg'
-githubUrl = githubUrl.replace('https://github.com/', '')
-githubUrl = 'https://raw.githubusercontent.com/' + githubUrl
-githubUrl = githubUrl + '/master'
-print('github 的链接 ： '+githubUrl)
-
-
-MDname = get_config()['md_name']
-
-
-def request_download(path,IMAGE_URL):
-    r = requests.get(IMAGE_URL, proxies = proxies)  #使用代理   ！！！！！！！ 也可以不用，我是境外的网站所以要用
+def request_download(path, IMAGE_URL):
+    """
+    :param path: 被存储到的路径
+    :param IMAGE_URL: 图片的url
+    :return:
+    """
+    if conf['proxy']:
+        r = requests.get(IMAGE_URL, proxies=proxies)  # 使用代理   ！！！！！！！ 也可以不用，我是境外的网站所以要用
+    else:
+        r = requests.get(IMAGE_URL)
     with open(path, 'wb') as f:
         f.write(r.content)
 
 
+def init():
+    global github_url
+    global md_name
+    global content
+    global md_file
+    global img_dir_pre
+    global md_name_hash
 
-with open(FileDir+'/sourcemd/' + MDname, 'r', encoding= 'utf-8', errors='ignore') as  f:
-    lines = f.readlines()
+    github_url = 'https://github.com/' + conf['username'] + '/' + conf['repository']
+    github_url = github_url.replace('https://github.com/', '')
+    github_url = 'https://raw.githubusercontent.com/' + github_url
+    github_url = github_url + '/master'
+    print('github image url 的链接前缀 ： ' + github_url)
 
+    md_name = conf['md_name']
+    md_name_hash = md5.my_md5(md_name)
+    img_dir_pre = FileDir + '/image/' + md_name_hash + '/'
 
+    # 新建可能需要的目录
+    os.makedirs(FileDir + '/image/', exist_ok=True)
+    os.makedirs(FileDir + '/copyMD/', exist_ok=True)
+    os.makedirs(FileDir + '/image/' + md_name_hash, exist_ok=True)
 
+    with open(FileDir + '/sourcemd/' + md_name, 'r', encoding='utf-8', errors='ignore') as f:
+        content = f.readlines()
 
-mdFile = open( FileDir +'/copyMD/' + MDname,'w',encoding= 'utf-8',)
-
-for i in lines:
-    try:
-        ans = re.findall('!.*(htt.*?)', i) # 检验有没有https图片
-        ans2 = re.findall(r'/.*png',i) # 检验有没有本地图片
-        if ans !=[]:
-
-            tmp = i.split('(')[1]
-            tmp = tmp.replace(')', '')
-            tmp = tmp.replace('\n', '')
-            name = tmp .replace('https://', '')
-            name = name .replace('http://', '')
-            name = name .replace('/', '')
-            name = name .replace('\n', '')
-            path = img_dir + md5.my_md5(name) +'.png'
-            request_download(path,tmp)
-            print(tmp , '已经保存到本地')
-            # print(path, 'path')
-            url = path.replace(FileDir, '')
-            # print(url)
-            url = githubUrl + url
-            print('图片在github上的链接 ： '+url)
-            i = i.replace(tmp, url)
-        elif ans2 != [] :
-            tmp = i.split('(')[1]
-            tmp = tmp.replace(')', '')
-            tmp = tmp.replace('\n', '')
-            path = img_dir + md5.my_md5(tmp) + '.png'
-            print(tmp)
-            img = Image.open(tmp)
-            img.save(path, 'png')
-
-            url = path.replace(FileDir, '')
-            url = githubUrl + url
-            print('图片在github上的链接 ： ' + url)
-            i = i.replace(tmp, url)
-
-        mdFile.write(i)
-    except Exception as e:
-        mdFile.write(i)
-        print(e)
+    md_file = open(FileDir + '/copyMD/' + md_name, 'w', encoding='utf-8', )
 
 
+def img_pro(img_url):
+    global github_url
+    global md_name
+    global content
+    global md_file
+    global img_dir_pre
+    global md_name_hash
+    new_local_img_path = img_dir_pre + md5.my_md5(img_url) +'.png'
+    new_github_img_path = github_url + '/image/' + md_name_hash +'/' + md5.my_md5(img_url) +'.png'
+    # http 图片
+    if re.findall('http', img_url) != []:
+        request_download(new_local_img_path , img_url)
 
+    # 本地的图片
+    else:
+        img = Image.open(img_url)
+        img.save(new_local_img_path, 'png')
+
+    return new_github_img_path
+
+
+def main():
+    global github_url
+    global md_name
+    global content
+    global md_file
+    global img_dir_pre
+
+    for line in content:
+        image_urls = re.findall(r'!\[.*\]\((.*)\)', line)  # 检验有没有图片,并提取出来
+        # print(image_urls)
+        if image_urls != []:
+            # print(image_urls)
+            git_url = img_pro(image_urls[0])
+            print(git_url)
+            line = line.replace(image_urls[0], git_url)
+            pass
+        else:
+            pass
+        md_file.write(line)
+
+
+if __name__ == '__main__':
+    init()
+    main()
